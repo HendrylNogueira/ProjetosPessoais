957aaa0d2852d421970af3fdb23a67939c3cdb12
Hendryl Nogueira <hendrylnogueira2508@gmail.com>
2025-02-27 08:48:33 -0300

    Iniciando front da aplicação

---
+++ ProjetoFullStack/FrontEndECommerce/FrontECommerce/src/styles/globalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const myGlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        body{
+            background-color: aliceblue;
+        }
+        
+    }
+`
+export default myGlobalStyles
